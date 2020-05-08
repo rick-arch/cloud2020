85938719f411ebd22ee7c8845b832c4c2594d21c
rick-arch <2405242004@qq.com>
2020-05-08 13:52:59 +0800

    rick

--- cloud-stream-rabbitmq-consumer8003/src/main/java/com/atguigu/springcloud/StreamMQConsumerApplication8803.java
+++ cloud-stream-rabbitmq-consumer8003/src/main/java/com/atguigu/springcloud/StreamMQConsumerApplication8803.java
@@ -6,11 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StreamMQConsumerApplication8003 {
-    private static final Logger logger = LoggerFactory.getLogger(StreamMQConsumerApplication8003.class);
+public class StreamMQConsumerApplication8803 {
+    private static final Logger logger = LoggerFactory.getLogger(StreamMQConsumerApplication8803.class);
 
     public static void main(String[] args) {
         logger.info("-----Stream Rabbitmq 服務消費者【8803】----已启动---------");
-        SpringApplication.run(StreamMQConsumerApplication8003.class,args);
+        SpringApplication.run(StreamMQConsumerApplication8803.class,args);
     }
 }
